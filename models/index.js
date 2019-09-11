4653ad24183197281676d6d528733d7495407966
brandiw <brandimichelle.0911@gmail.com>
2019-09-11 14:28:40 -0700

    Added the things

---
+++ models/index.js
@@ -0,0 +1,8 @@
+let mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern-sei-26', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+module.exports.User = require('./user')
