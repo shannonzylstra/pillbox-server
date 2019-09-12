3c5429fbc64c1dfaa8a7b81889e8f8e378dfa82a
brandiw <brandimichelle.0911@gmail.com>
2019-09-12 11:12:30 -0700

    Added login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,10 +3,36 @@ let jwt = require('jsonwebtoken')
 let db = require('../models')
 let router = require('express').Router()
 
+// POST /auth/login (find and validate user; send token)
 router.post('/login', (req, res) => {
-  res.send('STUB - POST /auth/login')
+  // Find the user by their email in the database
+  db.User.findOne({ email: req.body.email })
+  .then(user => {
+    // Make sure we have a user and that the user has a password
+    if (!user || !user.password) {
+      return res.status(404).send({ message: 'User not found' })
+    }
+
+    // Yay - we got a user. Let's check their password.
+    if (!user.isAuthenticated(req.body.password)) {
+      // Invalid credentials: wrong password
+      return res.status(406).send({ message: 'Not Acceptable: Invalid Credentials!' })
+    }
+
+    let token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+      expiresIn: 60 * 60 * 8 // 8 hours in seconds
+    })
+
+    res.send({ token })
+  })
+  .catch(err => {
+    // If something went wrong here, it's likely an issue with DB or DB setup. Or a typo.
+    console.log('Error in POST /auth/login', err)
+    res.status(503).send({ message: 'Something wrong, prob DB related. Or you made a typo. One of those.' })
+  })
 })
 
+// POST to /auth/signup (create user; generate token)
 router.post('/signup', (req, res) => {
   db.User.findOne({ email: req.body.email })
   .then(user => {
