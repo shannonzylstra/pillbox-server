2c1ea6586bd2a2c4251e399deee1be559a0a4a88
Brandi Williams <brandimichelle.0911@gmail.com>
2020-01-23 11:55:09 -0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
 let mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern-sei-26', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern-sei', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
+  useUnifiedTopology: true
 })
 
 module.exports.User = require('./user')
