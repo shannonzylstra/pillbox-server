47e2c7b213e2b0fc4b72c23cace9e757e944d2c3
shannonzylstra <shannonzylstra@gmail.com>
2020-01-31 14:33:51 -0800

    Sort of working AddDoseDialogue

--- controllers/userMedications.js
+++ controllers/userMedications.js
@@ -5,37 +5,6 @@ router.get('/', (req, res) => {
     db.UserMedication.find({ user: req.user._id})
     .populate('medication')
     .then(usermedications => {
-        // Promise<T>
-        // String -> ()
-        // then :: Promise<A> -> (A -> B) -> Promise<B>
-        // Promises.all :: [Promise<T>] -> Promise<[T]>
-        // console.log :: String -> ()
-        // userMedicationPromises :: [Promise<()>]
-        // Promises.all(userMedicationPromises) :: Promise<[()]>
-
-        // let userMedicationPromises = []
-        // usermedications.forEach(usermedication => {
-        //     userMedicationPromises.push(db.Medication.findOne({ _id: usermedication.medication })
-        //         .then(medication => {
-        //             let med = {
-        //                 medication: usermedication.medication,
-        //                 brand: medication.brand,
-        //                 generic: medication.generic,
-        //                 info: medication.info,
-        //                 image: medication.image,
-        //                 condition: usermedication.condition,
-        //                 doses: usermedication.doses
-        //             } 
-        //             // { medication, usermedication }
-        //             return med
-        //         })
-        //     )
-        // })
-        // Promise.all(userMedicationPromises)
-        // .then(results => {
-        //     res.send({ usermedications: results })
-        // })
-
         res.send({ usermedications })
     })
     .catch(err => {
@@ -71,7 +40,7 @@ router.post('/doses', (req, res) => {
         // res.send('success')
         console.log('med:', med)
         med.doses.push({
-            name: req.body.name,
+            name: req.body.name || 'morning',
             days: req.body.days || ['M','T','W','Th','F','Sa','S'],
             dosage: req.body.dosage || '1 dose',
             instructions: req.body.instructions || `Take ${req.body.dosage || '1 dose'} in the ${req.body.name}.` // potentially make more dynamic
